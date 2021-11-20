f9f3886e6c215ce915e932568754c62cd5228dcb
af072897 <af072897@cerner.net>
2021-11-20 11:17:51 -0600

    Updated Database and Session for progress update

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -7,8 +7,21 @@ public class Database {
         Class.forName("com.mysql.cj.jdbc.Driver");
         System.out.println("Driver loaded");
 
-        this.connection = DriverManager.getConnection("jdbc:mysql://localhost/javabook", user, pass);
-        System.out.println("Database connected");
+        System.out.println("Connecting to a selected database...");
+        // Open a connection
+        try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/profiles", user, pass);) {
+            System.out.println("Connected database successfully...");
+        } catch (SQLException e) {
+            try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/", user, pass);
+                Statement stmt = conn.createStatement();
+            ) {
+                String sql = "CREATE DATABASE profiles";
+                stmt.executeUpdate(sql);
+                System.out.println("Database created successfully...");
+            } catch (SQLException e2) {
+                e2.printStackTrace();
+            }
+        }
     }
 
     public Statement getStatement() throws SQLException {
--- src/main/java/Session.java
+++ src/main/java/Session.java
@@ -1,16 +1,30 @@
 import java.sql.SQLException;
 import java.sql.*;
 
+/*
+Pseudocode
+1. On session start, create database if one doesn't exist - Done
+2. In each profile function, create statement and corresponding SQL query to update database object
+    2a. Possibly make override executeQuery func for db object?
+3. Client-Server relations:
+    3a. Session interface?
+    3b. HelloApplication is client, Session is server
+    3c. Start dedicated server session object alongside client session object
+    3d. Would need to create dedicated driver file to create objects
+    3e. Client communicates user inputs to server, server formulates queries with input, injects into DB
+ */
+
 public class Session {
     Database database;
 
     public Session() throws SQLException, ClassNotFoundException{
-        database = new Database("alex", "tiger");
+        database = new Database("root", "L$839ppj!");
     }
 
     public void addProfile(Profile profile) {
         try {
             Statement statement = database.getStatement();
+
         }
         catch (SQLException ex){
             ex.printStackTrace();
@@ -36,6 +50,6 @@ public class Session {
     public static void main(String[] args) throws SQLException, ClassNotFoundException{
         Session session = new Session();
         Profile alex = new Profile("alex", "111-222-3344", "totallylegit@gmail.com", "123 Oak Street");
-        session.addProfile(alex);
+//        session.addProfile(alex);
     }
 }
