2b4e478ea304806dc195581f3effb19c72106c42
Eli Monnig <35583627+Stick-2357@users.noreply.github.com>
2021-12-03 14:09:20 -0600

    select returns list of selected profiles

--- src/main/java/team/pages/TripPageHandler.java
+++ src/main/java/team/pages/TripPageHandler.java
@@ -2,6 +2,7 @@ package team.pages;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -9,8 +10,10 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import team.Profile;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class TripPageHandler implements EventHandler<ActionEvent> {
     @Override
@@ -18,24 +21,31 @@ public class TripPageHandler implements EventHandler<ActionEvent> {
         Stage newStage = new Stage();
 
         // TODO: populate from getAllProfiles
-        ArrayList<CheckBox> children = new ArrayList<>();
+        VBox childrenVBox = new VBox();
         for (int i = 0; i < RootApplication.roster.size(); i++) {
             Profile child = RootApplication.roster.get(i);
             CheckBox childCheckbox = new CheckBox(child.getName());
-            children.add(childCheckbox);
+            childCheckbox.setId(String.valueOf(child.getId()));
+            childrenVBox.getChildren().add(childCheckbox);
         }
 
         Button select = new Button("Generate Route");
-
         select.setOnAction((event) -> {
-            // TODO: Call GetOptimizedRoute passing selected child locations
+            ArrayList<Profile> selectedChildren = new ArrayList<>();
+            for (Node node : childrenVBox.getChildren()) {
+                if (node.getClass() == CheckBox.class) { // deals with non-checkbox nodes
+                    CheckBox checkBox = (CheckBox) node;
+                    if (checkBox.isSelected()) {
+                        int childID = Integer.parseInt(checkBox.getId());
+                        Profile selectedChild = RootApplication.roster.stream().filter(c -> c.getId() == childID).collect(Collectors.toList()).get(0);
+                        selectedChildren.add(selectedChild);
+                    }
+                }
+            }
+            System.out.println(selectedChildren);
         });
 
-        VBox pane4 = new VBox();
-        for (CheckBox checkBox : children) {
-            pane4.getChildren().add(checkBox);
-        }
-        pane4.getChildren().add(select);
+        VBox pane4 = new VBox(childrenVBox, select);
 
         Scene scene = new Scene(pane4);
         newStage.setTitle("Select Member's for Trip");
